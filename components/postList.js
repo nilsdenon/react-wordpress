35bba211be89abdba1ad6a878edee36613fe15a0
Damien Pierre <kalumbox@gmail.com>
2019-03-05 12:12:11 +0100

    srcset implementation

--- components/postList.js
+++ components/postList.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import Image from "react-simple-image";
 
 class PostList extends Component {
     constructor(props) {
@@ -19,18 +20,60 @@ class PostList extends Component {
         });
     }
     render() {
+        const baseURL = "https://damienpierre.com";
         return (
             <div>
                 {this.state.posts.map(post => (
                     <Link to={`/${post.slug}`} key={post.id}>
                         <div className="card" key={post.id}>
                             <div className="card-content">
-                                <h3>{post.title.rendered}</h3>
+                                <h1>{post.title.rendered}</h1>
                                 <p>{post.excerpt.rendered}</p>
                                 <ul>
                                     <li>{post.acf.project_client}</li>
                                     <li>{post.acf.project_type}</li>
                                 </ul>
+
+                                <Image
+                                    alt={post.acf.hero_background_image.alt}
+                                    className="additional-className"
+                                    src={
+                                        baseURL +
+                                        post.acf.hero_background_image.url
+                                    }
+                                    srcSet={{
+                                        "2048w":
+                                            baseURL +
+                                            (post.acf.hero_background_image.sizes.large
+                                                .split(".")
+                                                .slice(0, -1) +
+                                                "@2x.jpg"),
+                                        "1024w":
+                                            baseURL +
+                                            post.acf.hero_background_image.sizes
+                                                .large,
+                                        "768w":
+                                            baseURL +
+                                            post.acf.hero_background_image.sizes
+                                                .medium,
+                                        "480w":
+                                            baseURL +
+                                            post.acf.hero_background_image.sizes
+                                                .smartphone_large,
+                                        "320w":
+                                            baseURL +
+                                            post.acf.hero_background_image.sizes
+                                                .smartphone_small,
+                                        "1440w":
+                                            baseURL +
+                                            post.acf.hero_background_image.sizes
+                                                .backdrop,
+                                        "1024w":
+                                            baseURL +
+                                            post.acf.hero_background_image.sizes
+                                                .large
+                                    }}
+                                />
                             </div>
                         </div>
                     </Link>
